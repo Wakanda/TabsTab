6820a1c0f754c5f093250399ee0000a2b47a4f69
Stéphane Puybareau <stephane.puybareau@4d.com>
2014-10-31 16:13:26 +0100

    formating issues in widget.js

--- widget.js
+++ widget.js
@@ -4,26 +4,31 @@ WAF.define('TabsTab', ['waf-core/widget', 'Button'], function(widget, Button) {
     var TabsTab = widget.createSubWidget('TabsTab', {
         tagName: 'li',
         init: function() {
-            this.node.innerHTML = this.value();
-            this.closeButton.onChange(function() {
+            var changeCloseButton = function() {
                 if(this.closeButton()) {
                     var button = this.getPart('closeButton');
                     if(!button) {
                         button = new Button({ value: 'X' });
-                    	this.setPart('closeButton', button);
+                        this.setPart('closeButton', button);
                     }
                     //this.addClass('waf-' + this.kind.toLowerCase() + '-closable');
-                    button.addClass('waf-tabview2-closeTab')
+                    button.addClass('waf-tabs-closeTab');
                     button.removeClass('waf-button');
                 } else {
                     this.setPart('closeButton', undefined);
                     //this.removeClass('waf-' + this.kind.toLowerCase() + '-closable');
                 }
-            });
+            };
+            this.closeButton.onChange(changeCloseButton);
+
+            this.node.innerHTML = this.value();
+            changeCloseButton.call(this);
         },
         value: widget.property({
             onChange: function() {
+                var button = this.getPart('closeButton');
                 this.node.innerHTML = this.value();
+                this.setPart('closeButton', button);
             },
             defaultValueCallback: function() {
                 return this.node.innerHTML;
